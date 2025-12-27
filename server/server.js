fd448f06a8e6f26df090a34dcf94f7edbdeee582
Titus Tissot <titus.tissot@melantech.de>
2025-12-27 02:49:06 +0100

    Procfile

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const { truncateSync } = require('fs');
 // const createGame = require('./create-game');
 
 const app = express();
